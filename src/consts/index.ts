d913b2c9cd93401ade58f356b7c3f09f8a39638c
Adam Gołąb <adam.golab@pickatale.com>
2022-11-14 21:09:20 +0100

    feat: add colors consts

---
+++ src/consts/index.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  textSecondary: '#bdbdbd',
+  textPrimary: '#217eb1',
+  backgroundPrimary: '#1aadf8',
+  backgroundPrimaryRGBA: 'rgba(26, 173, 248, 0.3)',
+  lines: '#e5e5e5',
+  white: '#ffffff',
+};
